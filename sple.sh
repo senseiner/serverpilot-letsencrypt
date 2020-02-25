3db847ba10e6442d92f30da05f22e103addb9d98
Alejandro Seoane <senseiner@users.noreply.github.com>
2020-02-25 15:21:05 -0500

    Delete TLSv1.1

--- sple.sh
+++ sple.sh
@@ -194,7 +194,7 @@ echo "    ssl_session_cache shared:SSL:20m;" | sudo tee -a $configfile
 echo "    ssl_session_tickets off;" | sudo tee -a $configfile 
 echo "" | sudo tee -a $configfile 
 echo "    # modern configuration" | sudo tee -a $configfile 
-echo "    ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;" | sudo tee -a $configfile 
+echo "    ssl_protocols TLSv1.2 TLSv1.3;" | sudo tee -a $configfile 
 echo "    ssl_prefer_server_ciphers on;" | sudo tee -a $configfile 
 echo "" | sudo tee -a $configfile 
 echo "    ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK';" | sudo tee -a $configfile 
